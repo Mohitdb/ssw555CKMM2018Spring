47c3028f8349d5d9d1d66f685f29eaba16a04f4a
csquilla567 <36014412+csquilla567@users.noreply.github.com>
2018-02-11 20:27:05 -0500

    add getters and setters

--- Family.java
+++ Family.java
@@ -1,4 +1,4 @@
-package gedDistributor;
+//package gedDistributor;
 
 import java.util.ArrayList;
 
@@ -25,4 +25,67 @@ public class Family
 		children = new ArrayList<String>();
 	}
 
+	public String getFamID() {
+		return famID;
+	}
+
+	public void setFamID(String famID) {
+		this.famID = famID;
+	}
+
+	public String getMarried() {
+		return married;
+	}
+
+	public void setMarried(String married) {
+		this.married = married;
+	}
+
+	public boolean isDivorced() {
+		return divorced;
+	}
+
+	public void setDivorced(boolean divorced) {
+		this.divorced = divorced;
+	}
+
+	public String getHusbID() {
+		return husbID;
+	}
+
+	public void setHusbID(String husbID) {
+		this.husbID = husbID;
+	}
+
+	public String getHusbName() {
+		return husbName;
+	}
+
+	public void setHusbName(String husbName) {
+		this.husbName = husbName;
+	}
+
+	public String getWifeID() {
+		return wifeID;
+	}
+
+	public void setWifeID(String wifeID) {
+		this.wifeID = wifeID;
+	}
+
+	public String getWifeName() {
+		return wifeName;
+	}
+
+	public void setWifeName(String wifeName) {
+		this.wifeName = wifeName;
+	}
+
+	public ArrayList<String> getChildren() {
+		return children;
+	}
+
+	public void setChildren(ArrayList<String> children) {
+		this.children = children;
+	}
 }
