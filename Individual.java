a2aaa3f468e3344a2975031a6cf6f323bb6b4cca
csquilla567 <36014412+csquilla567@users.noreply.github.com>
2018-02-11 20:27:33 -0500

    add getters and setters

--- Individual.java
+++ Individual.java
@@ -1,4 +1,4 @@
-package gedDistributor;
+//package gedDistributor;
 
 import java.util.ArrayList;
 
@@ -25,6 +25,78 @@ public class Individual
 			death = "";
 			child = new ArrayList<String>();
 			spouse = new ArrayList<String>();
+		}
+
+		public String getIndividualID() {
+			return individualID;
+		}
+
+		public void setIndividualID(String individualID) {
+			this.individualID = individualID;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getGender() {
+			return gender;
+		}
+
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+
+		public String getBirth() {
+			return birth;
+		}
+
+		public void setBirth(String birth) {
+			this.birth = birth;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public boolean isAlive() {
+			return alive;
+		}
+
+		public void setAlive(boolean alive) {
+			this.alive = alive;
+		}
+
+		public String getDeath() {
+			return death;
+		}
+
+		public void setDeath(String death) {
+			this.death = death;
+		}
+
+		public ArrayList<String> getChild() {
+			return child;
+		}
+
+		public void setChild(ArrayList<String> child) {
+			this.child = child;
+		}
+
+		public ArrayList<String> getSpouse() {
+			return spouse;
+		}
+
+		public void setSpouse(ArrayList<String> spouse) {
+			this.spouse = spouse;
 		}
 		
 }
