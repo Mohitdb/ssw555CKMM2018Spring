010559c88cce23b135d6172c202ee20dd3ff5790
karan <karuaan@gmail.com>
2018-02-21 17:16:52 -0500

    Find the ages

--- GedcomParse.java
+++ GedcomParse.java
@@ -387,6 +387,40 @@ public class GedcomParse {
                 s1.marriageAfter14(indiHash, famHash);
 				String resBirthBeforeDeath=m.birthBeforeDeath(indiHash);
         }
+	
+	public static String dateNumber(String monthName)
+	{
+		switch (monthName)
+		{
+			case "JAN":
+				return "01";
+			case "FEB":
+				return "02";
+			case "MAR":
+				return "03";
+			case "APR":
+				return "04";
+			case "MAY":
+				return "05";
+			case "JUN":
+				return "06";
+			case "JUL":
+				return "07";
+			case "AUG":
+				return "08";
+			case "SEP":
+				return "09";
+			case "OCT":
+				return "10";
+			case "NOV":
+				return "11";
+			case "DEC":
+				return "12";
+			default:
+				return "00";
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		// TODO code application logic here
 		parse();
