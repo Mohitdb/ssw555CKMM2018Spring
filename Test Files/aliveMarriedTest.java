3ea68af7f9d070066074e0dd0eb665febd9bd9f6
csquilla567 <36014412+csquilla567@users.noreply.github.com>
2018-03-05 11:07:09 -0500

    add test for us 29 and 33

--- Test Files/aliveMarriedTest.java
+++ Test Files/aliveMarriedTest.java
@@ -48,17 +48,44 @@ class aliveMarriedTest {
 		id4.add("F");
 		id4.add("5 NOV 1970");
 		id4.add("");
-		id4.add("25");
+		id4.add("45");
 		indiHash.put("I11", id4);
 		
 		ArrayList<String> id5 = new ArrayList<String>();
 		id5.add("I5");
 		id5.add("Sean Sweeney");
 		id5.add("M");
-		id5.add("12 NOV 1935");
+		id5.add("12 NOV 1965");
 		id5.add("");
-		id5.add("35");
+		id5.add("55");
 		indiHash.put("I5", id5);
+		
+		ArrayList<String> id6 = new ArrayList<String>();
+		id6.add("I6");
+		id6.add("John Sweeney");
+		id6.add("M");
+		id6.add("3 NOV 1970");
+		id6.add("11 SEP 2008");
+		id6.add("50");
+		indiHash.put("I6", id6);
+		
+		ArrayList<String> id7 = new ArrayList<String>();
+		id7.add("I7");
+		id7.add("Kristine Sweeney");
+		id7.add("F");
+		id7.add("20 NOV 1970");
+		id7.add("11 SEP 2008");
+		id7.add("50");
+		indiHash.put("I7", id7);
+		
+		ArrayList<String> id8 = new ArrayList<String>();
+		id8.add("I8");
+		id8.add("Sammy Sweeney");
+		id8.add("F");
+		id8.add("18 Dec 2000");
+		id8.add("");
+		id8.add("14");
+		indiHash.put("I8", id8);
 		
 		ArrayList<String> fam1 = new ArrayList<String>();
 	
@@ -67,6 +94,9 @@ class aliveMarriedTest {
 		fam1.add("Lucas Gallen");
 		fam1.add("I11");
 		fam1.add("Nicole Jason");
+		fam1.add("19 Dec 1997");
+		fam1.add("");
+		fam1.add("");
 		famHash.put("F6", fam1);
 		
 		ArrayList<String> fam2 = new ArrayList<String>();
@@ -75,20 +105,42 @@ class aliveMarriedTest {
 		fam2.add("Justin Sweeney");
 		fam2.add("I2");
 		fam2.add("Pam Janson");
+		fam2.add("7 Nov 1999");
+		fam2.add("");
+		fam2.add("");
 		famHash.put("F1", fam2);
+		
+		ArrayList<String> fam3 = new ArrayList<String>();
+		fam3.add("F3");
+		fam3.add("I6");
+		fam3.add("John Sweeney");
+		fam3.add("I7");
+		fam3.add("Kristine Sweeney");
+		fam3.add("7 Jul 1997");
+		fam3.add("");
+		fam3.add("I8");
+		famHash.put("F3", fam3);
 		
 		AliveMarried am = new AliveMarried(indiHash, famHash);
 		
 		//System.out.println(am.getAliveMarried());
 		//System.out.println(am.getAliveSingle());
 
-		assertEquals("[[I1, Justin Sweeney, M, 11 SEP 1963, , 48], [I11, Nicole Janson, F, 5 NOV 1970, , 25], [I2, Pam Janson, F, 7 APR 1965, , 45]]", am.getAliveMarried().toString());
+		//System.out.println(id2);
+		
+		assertEquals("[[I1, Justin Sweeney, M, 11 SEP 1963, , 48], [I11, Nicole Janson, F, 5 NOV 1970, , 45], [I2, Pam Janson, F, 7 APR 1965, , 45]]", am.getAliveMarried().toString());
 		assertFalse(am.getAliveMarried().contains(id2)); 
 		assertFalse(am.getAliveMarried().contains(id5)); 
 		assertTrue(am.getAliveMarried().contains(id1)); 
-		assertTrue(am.getAliveMarried().contains(id4)); 
-		assertEquals("[[I5, Sean Sweeney, M, 12 NOV 1935, , 35]]", am.getAliveSingle().toString());
+		assertTrue(am.getAliveMarried().contains(id3)); 
+		assertEquals("[[I5, Sean Sweeney, M, 12 NOV 1965, , 55]]", am.getAliveSingle().toString());
 		assertTrue(am.getAliveSingle().contains(id5)); 
+		assertEquals("[[I12, Lucas Gallen, M, 7 JUL 1965, 11 SEP 2001, 45], [I6, John Sweeney, M, 3 NOV 1970, 11 SEP 2008, 50], [I7, Kristine Sweeney, F, 20 NOV 1970, 11 SEP 2008, 50]]", am.getDeceased().toString());
+		assertTrue(am.getDeceased().contains(id2)); 
+		assertTrue(am.getDeceased().contains(id6)); 
+		assertTrue(am.getDeceased().contains(id7)); 
+		assertEquals("[[I8, Sammy Sweeney, F, 18 Dec 2000, , 14]]", am.getOrphans().toString());
+		assertTrue(am.getOrphans().contains(id8)); 
 	}
 
 }
