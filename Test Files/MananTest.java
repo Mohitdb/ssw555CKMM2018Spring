f90311b15cb71f38a43a3f11dd4db89e0b07a1bb
Manan Satra <msatra@stevens.edu>
2018-03-05 13:46:13 -0500

    .ged changes in name and test file commit
    Changed 2 names in .ged files.
    Test file updated with test for lessThan150 and test including after refactoring.

--- Test Files/MananTest.java
+++ Test Files/MananTest.java
@@ -1,43 +1,45 @@
+
 package GedcomParse;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class MananTest
 {
-
-    @Test
+//    mananRefractored instance=new mananRefractored();
+    Manan instance=new Manan();
+   @Test
     public void testBirthBeforeDeath()
     {
         String res = "";
-        System.out.println("birthBeforeDeath");
+        System.out.println("==================Test for US03: Birth Before Marriage==================");
 
-        Manan instance = new Manan();
         String testID[] =
         {
-            "T1", "T2", "T3","T4","T5","T6","T7"
+            "T1", "T2", "T3",
         };
         String testName[] =
         {
-            "ABC", "DEF", "GHI","JKL","MNO","PQR","LMN"
+            "ABC", "DEF", "GHI",
         };
         String testGender[] =
         {
-            "M", "M", "F","F","M","F","M"
+            "M", "M", "F",
         };
         String testBirth[] =
         {
-            "09 FEB 2000", "25 FEB 2007", "15 SEP 1990","1 JAN 2017","17 MAY 2014","",""
+            "09 FEB 2000", "12 MAR 2007", "15 SEP 1990",
         };
         String testDeath[] =
         {
-            "08 JAN 1999", "12 MAR 2007", "14 SEP 1990","","18 JUN 2015","","20 MAR 1999"
+            "08 JAN 1999", "25 FEB 2007", "14 SEP 1990",
         };
         String expected[] =
         {
-            "INCORRECT", "INCORRECT", "INCORRECT","CORRECT","CORRECT","INCORRECT","INCORRECT"
+            "INCORRECT", "INCORRECT", "INCORRECT",
         };
         String[] resArray = new String[testID.length];
         HashMap<String, ArrayList<String>> hashIndi = new HashMap<>();
@@ -53,13 +55,70 @@ public class MananTest
             tempArrayList.add(testDeath[i]);
             hashIndi.putIfAbsent(testID[i], tempArrayList);
             res = instance.birthBeforeDeath(hashIndi);
-//            System.out.println("Expected Output: "+expected[i]);
-//            System.out.println("Actual Output :"+res);
-//            System.out.println();
             resArray[i] = res;
         }
         Assert.assertArrayEquals(expected, resArray);
 
     }
 
+    @Test
+    public void testLessThan150()
+    {
+        String resLessThan150 = "";
+        System.out.println("==================Test for US07: lessThan150==================");
+        FindAges fa = new FindAges();
+        String testID[] =
+        {
+            "T1", "T2", "T3"
+        };
+        String testName[] =
+        {
+            "ABC", "DEF", "GHI"
+        };
+        String testGender[] =
+        {
+            "M", "M", "F"
+        };
+        String testBirth[] =
+        {
+            "9 FEB 1800", "12 MAR 1700", "1 JUN 1802"
+        };
+        String testDeath[] =
+        {
+            "8 JAN 2000", "1 JAN 2001", "",
+        };
+        
+        // had to add this section of code in test file after refractoring
+        String testAge[] = new String[testID.length];
+        for (int i = 0; i < testBirth.length; i++)
+        {
+            Integer age = fa.FindAge(testBirth[i], testDeath[i]);
+            testAge[i] = age.toString();
+        }
+        //end of section that needed to be added
+        
+        String expectedLT150[] =
+        {
+            "INCORRECT", "INCORRECT", "INCORRECT"
+        };
+        String[] resArrayLT150 = new String[testID.length];
+        HashMap<String, ArrayList<String>> hashIndi = new HashMap<>();
+        ArrayList<String> tempArrayList = new ArrayList<>();
+        for (int i = 0; i < testID.length; i++)
+        {
+            hashIndi.clear();
+            tempArrayList.clear();
+            tempArrayList.add(testID[i]);
+            tempArrayList.add(testName[i]);
+            tempArrayList.add(testGender[i]);
+            tempArrayList.add(testBirth[i]);
+            tempArrayList.add(testDeath[i]);
+            tempArrayList.add(testAge[i]); //this line needed to be added
+            hashIndi.putIfAbsent(testID[i], tempArrayList);
+            resLessThan150 = instance.lessThan150(hashIndi);
+            resArrayLT150[i] = resLessThan150;
+        }
+        Assert.assertArrayEquals(expectedLT150, resArrayLT150);
+
+    }
 }
