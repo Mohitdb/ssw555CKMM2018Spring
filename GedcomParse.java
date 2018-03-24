2d105ba64fb499ef36c0b6d055e6ed4997c79aa8
Mohitdb <36172336+Mohitdb@users.noreply.github.com>
2018-03-24 19:28:41 -0400

    Minor change

--- GedcomParse.java
+++ GedcomParse.java
@@ -49,13 +49,11 @@ public class GedcomParse
         // TODO: Change path for our testing file
         String carolinePath = "C:\\Users\\Caroline Squillante\\workspace\\gedDistributor\\src\\sprint2.ged";
         String mananPath = "D:\\HIGHER STUDIES\\Stevens\\MS SEM 2\\CS 555 Agile methods for software dev\\GedcomParse\\Gedcom Files\\Manan\\mananSprint3.ged";
-<<<<<<< HEAD
+
         String mohitPath = "C:\\Users\\mohit\\Documents\\NetBeansProjects\\GedcomParse\\build\\classes\\gedcomparse\\US18.ged";
         String karanPath = "C:\\Users\\Class2018\\Desktop\\Agile\\Group Work\\GitHub Here\\ssw555CKMM2018Spring\\Gedcom Files\\sprint2.ged";
-=======
-        String mohitPath = "C:\\Users\\mohit\\Documents\\NetBeansProjects\\GedcomParse\\build\\classes\\gedcomparse\\Sprint1.ged";
-        String karanPath = "C:\\Users\\Class2018\\Desktop\\Agile\\Group Work\\GitHub Here\\ssw555CKMM2018Spring\\ssw555CKMM2018Spring\\Gedcom Files\\KaranSprint3.ged";
->>>>>>> 27763d7e6e77b7c24eb49fd4b58acc738ebbd954
+
+
         
  //      FileReader fileReader = new FileReader(karanPath);
 //        FileReader fileReader = new FileReader(mananPath);
