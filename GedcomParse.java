48de2e9c61044f9f188dae03b67f1c05319c902b
csquilla <csquilla@stevens.edu>
2018-03-26 16:41:21 -0400

    adding us 32 and 15 to gedcom

--- GedcomParse.java
+++ GedcomParse.java
@@ -399,6 +399,9 @@ public class GedcomParse
         
         //--------------------------Carolines Space-------------------------
         
+        Siblings sb = new Siblings(famHash, indiHash);
+        sb.checkSiblings();
+        sb.listMultBirths();
         
         
         
