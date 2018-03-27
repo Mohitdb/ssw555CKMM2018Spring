c3208c1ae19f86595f651baeae5d6b59ac43884e
csquilla <csquilla@stevens.edu>
2018-03-27 09:45:34 -0400

    sprint 3 test

--- GedcomParse.java
+++ GedcomParse.java
@@ -47,17 +47,17 @@ public class GedcomParse
         
         //read file
         // TODO: Change path for our testing file
-        String carolinePath = "C:\\Users\\Caroline Squillante\\workspace\\gedDistributor\\src\\sprint2.ged";
+        String carolinePath = "C:\\Users\\Caroline Squillante\\workspace\\gedDistributor\\src\\sprint3.ged";
         String mananPath = "D:\\HIGHER STUDIES\\Stevens\\MS SEM 2\\CS 555 Agile methods for software dev\\GedcomParse\\Gedcom Files\\Manan\\mananSprint3.ged";
         String mohitPath = "C:\\Users\\mohit\\Documents\\NetBeansProjects\\GedcomParse\\build\\classes\\gedcomparse\\US42.ged";
         String karanPath = "C:\\Users\\Class2018\\Desktop\\Agile\\Group Work\\GitHub Here\\ssw555CKMM2018Spring\\ssw555CKMM2018Spring\\Gedcom Files\\KaranSprint3.ged";
 
 
         
-         FileReader fileReader = new FileReader(karanPath);
+//         FileReader fileReader = new FileReader(karanPath);
 //       FileReader fileReader = new FileReader(mananPath);
 //       FileReader fileReader = new FileReader(mohitPath);
-//       FileReader fileReader = new FileReader(carolinePath);
+       FileReader fileReader = new FileReader(carolinePath);
         
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         System.out.println("******************** Karan's User story US21: Correct Gender for Role ********************");
